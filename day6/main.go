557bc84ac96d25668120ed2163442ba8bea0d511
aocthicc <75556797+aocthicc@users.noreply.github.com>
2020-12-06 02:08:38 -0500

    Finished day6 part 2

--- day6/main.go
+++ day6/main.go
@@ -14,7 +14,28 @@ func (g *Group) AddPerson(person Person) {
 	g.People = append(g.People, person)
 }
 
-func (g *Group) GetTotalUniqueYesAnswers() int {
+func (g *Group) GetUnanimousAnswersCount() int {
+	answers := make(map[string]int)
+	for _, person := range g.People {
+		for _, answer := range person.GetAnswers() {
+			if _, ok := answers[answer]; !ok {
+				answers[answer] = 1
+			} else {
+				answers[answer] += 1
+			}
+		}
+	}
+	unanimousAnswers := 0
+	for _, answerCount := range answers {
+		if answerCount == len(g.People) {
+			unanimousAnswers += 1
+		}
+	}
+
+	return unanimousAnswers
+}
+
+func (g *Group) GetUniqueYesAnswersCount() int {
 	answers := make(map[string]bool)
 	for _, person := range g.People {
 		for _, answer := range person.GetAnswers() {
@@ -43,10 +64,13 @@ func main() {
 
 	groups := mapIntoGroups(string(data))
 	totalYesAnswers := 0
+	totalUnanimousAnswers := 0
 	for _, group := range groups {
-		totalYesAnswers += group.GetTotalUniqueYesAnswers()
+		totalYesAnswers += group.GetUniqueYesAnswersCount()
+		totalUnanimousAnswers += group.GetUnanimousAnswersCount()
 	}
 	fmt.Println(totalYesAnswers)
+	fmt.Println(totalUnanimousAnswers)
 }
 
 func mapIntoGroups(data string) []Group {
@@ -54,10 +78,10 @@ func mapIntoGroups(data string) []Group {
 	var current Group
 	parsed := strings.Split(data, "\r")
 	for i, line := range parsed {
-		if i == len(parsed) - 1 {
+		if i == len(parsed)-1 {
 			current.AddPerson(Person{Answers: strings.TrimSpace(line)})
 		}
-		if line == "\n" || i == len(parsed) -1 {
+		if line == "\n" || i == len(parsed)-1 {
 			groups = append(groups, current)
 			current = Group{}
 		} else {
@@ -66,4 +90,3 @@ func mapIntoGroups(data string) []Group {
 	}
 	return groups
 }
-
--- day6/main_test.go
+++ day6/main_test.go
@@ -4,16 +4,25 @@ import "testing"
 
 func TestGroupGetUniqueAnswers(t *testing.T) {
 	group := Group{People: []Person{{"abc"}, {"abz"}}}
-	ans := group.GetTotalUniqueYesAnswers()
+	ans := group.GetUniqueYesAnswersCount()
 	if ans != 4 {
-		t.Errorf("GetTotalUniqueYesAnswers(\"abc\", \"abz\") = %d, want 4", ans)
+		t.Errorf("GetUniqueYesAnswersCount() = %d, want 4", ans)
 	}
 
 
 	group = Group{People: []Person{{"vs"}, {"lqn"}, {"ti"}, {"uvl"}}}
-	ans = group.GetTotalUniqueYesAnswers()
+	ans = group.GetUniqueYesAnswersCount()
 
 	if ans != 8 {
-		t.Errorf("GetTotalUniqueYesAnswers() = %d, want 8", ans)
+		t.Errorf("GetUniqueYesAnswersCount() = %d, want 8", ans)
+	}
+}
+
+func TestGroup_GetGroupUnanimousAnswers(t *testing.T) {
+	group := Group{People: []Person{{"ab"}, {"ac"}}}
+
+	ans := group.GetUnanimousAnswersCount()
+	if ans != 1 {
+		t.Errorf("GetUniqueYesAnswersCount() = %d, want 1", ans)
 	}
 }
